616f267f403d96a8ba70af3d073ea717327a4ae7
Wallas9_3 <drame.sissako001@gmail.com>
2022-07-21 15:53:39 +0200

    user can now hide/show the sidbar

--- src/App.vue
+++ src/App.vue
@@ -293,16 +293,27 @@ const cols = ref([
   },
 ]);
 
-const show = ref(false);
+const showSidebar = ref(true);
+const showModal = ref(false);
 const boards = ref([]);
+
+function onHideSidebar() {
+  showSidebar.value = false;
+}
+
+function onShowSidebar() {
+  showSidebar.value = true;
+}
 </script>
 
 <template>
   <!-- Ajouter un wrapper pour l'animation de la modal -->
   <!-- <button @click="show = !show">CLICK</button> -->
+  <!-- MODALS -->
   <Teleport to="body">
-    <ktm-modal :show="false"></ktm-modal>
+    <ktm-modal :show="showModal"></ktm-modal>
   </Teleport>
+  <!-- MODALS -->
   <div class="home-page home-page--dark-mode">
     <!-- HEADER -->
     <KTMHeader />
@@ -310,7 +321,9 @@ const boards = ref([]);
     <!-- MAIN -->
     <main>
       <!-- TABLET AND DESKTOP SIDEBAR -->
-      <KTMSidebar />
+      <Transition name="sidebar">
+        <KTMSidebar v-if="showSidebar" @hide-sidebar="onHideSidebar" />
+      </Transition>
       <!-- TABLET AND DESKTOP SIDEBAR -->
       <!-- BOARD CONTENT -->
       <section class="boards">
@@ -320,6 +333,19 @@ const boards = ref([]);
         </div>
       </section>
       <!-- BOARD CONTENT -->
+      <!-- HIDDEN SIDEBAR -->
+      <Transition name="hidden-sidebar">
+        <div v-if="!showSidebar" @click="onShowSidebar" class="hidden-sidebar">
+          <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M8.522 11.223a4.252 4.252 0 0 1-3.654-5.22l3.654 5.22ZM9 12.25A8.685 8.685 0 0 1 1.5 8a8.612 8.612 0 0 1 2.76-2.864l-.86-1.23A10.112 10.112 0 0 0 .208 7.238a1.5 1.5 0 0 0 0 1.524A10.187 10.187 0 0 0 9 13.75c.414 0 .828-.025 1.239-.074l-1-1.43A8.88 8.88 0 0 1 9 12.25Zm8.792-3.488a10.14 10.14 0 0 1-4.486 4.046l1.504 2.148a.375.375 0 0 1-.092.523l-.648.453a.375.375 0 0 1-.523-.092L3.19 1.044A.375.375 0 0 1 3.282.52L3.93.068a.375.375 0 0 1 .523.092l1.735 2.479A10.308 10.308 0 0 1 9 2.25c3.746 0 7.031 2 8.792 4.988a1.5 1.5 0 0 1 0 1.524ZM16.5 8a8.674 8.674 0 0 0-6.755-4.219A1.75 1.75 0 1 0 12.75 5v-.001a4.25 4.25 0 0 1-1.154 5.366l.834 1.192A8.641 8.641 0 0 0 16.5 8Z"
+              fill="#828FA3"
+            />
+          </svg>
+        </div>
+      </Transition>
+
+      <!-- HIDDEN SIDEBAR -->
     </main>
     <!-- MAIN -->
   </div>
@@ -335,6 +361,8 @@ const boards = ref([]);
 .home-page {
   height: 100%;
   main {
+    position: relative;
+
     .no-boards {
       display: flex;
       flex-direction: column;
@@ -357,6 +385,25 @@ const boards = ref([]);
         padding: f.toRem(15, 12) f.toRem(18, 12) f.toRem(14, 12) f.toRem(17, 12);
       }
     }
+
+    .hidden-sidebar {
+      cursor: pointer;
+      position: absolute;
+      bottom: f.toRem(32, 12);
+      left: 0;
+      padding: f.toRem(19, 12) f.toRem(22, 12) f.toRem(18.78, 12)
+        f.toRem(18, 12);
+      border-radius: 0px 100px 100px 0px;
+      background-color: c.$MainPurple;
+
+      & > svg > path {
+        fill: c.$White;
+      }
+
+      &:hover {
+        background-color: c.$MainPurpleHover;
+      }
+    }
   }
 }
 .home-page--light-mode {
@@ -365,6 +412,89 @@ const boards = ref([]);
 .home-page--dark-mode {
   background-color: c.$VeryDarkGrey;
 }
+
+/* Animations */
+
+/* SIDEBAR ANIMATIONS */
+.sidebar-enter-active {
+  animation: s-translate-in 0.6s;
+}
+.sidebar-leave-active {
+  animation: s-translate-out 0.5s;
+}
+@keyframes s-translate-in {
+  0% {
+    transform: translateX(-261px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes s-translate-out {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-261px);
+  }
+}
+
+@include m.breakpoint-up(large) {
+  @keyframes s-translate-in {
+    0% {
+      transform: translateX(-301px);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes s-translate-out {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-301px);
+    }
+  }
+}
+
+/* SIDEBAR ANIMATIONS */
+
+/* HIDDEN SIDEBAR ANIMATIONS */
+
+.hidden-sidebar-enter-active {
+  animation: hs-translate-in 0.6s;
+}
+.hidden-sidebar-leave-active {
+  animation: hs-translate-out 0.5s;
+}
+@keyframes hs-translate-in {
+  0% {
+    transform: translateX(-56px);
+  }
+  60% {
+    transform: translateX(0);
+  }
+  90% {
+    transform: translateX(-5px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes hs-translate-out {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-56px);
+  }
+}
+
+/* HIDDEN SIDEBAR ANIMATIONS */
 
 @include m.breakpoint-up(medium) {
   .home-page {
