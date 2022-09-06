c5bed5380e153b2c39db5882eb39d2105f04b437
wallas938 <drame.sissako001@gmail.com>
2022-09-07 00:59:14 +0200

    sign in feature with mongodb is ready

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,53 @@
 import { useLayoutStore } from "./../stores/layout";
-import type { KtmUser, UserMetaData } from "@/model";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-  onAuthStateChanged,
-} from "firebase/auth";
+import type { KtmUser } from "@/model";
+
 import { useAuthStore } from "@/stores/auth";
 
-const registerStandard = async (email: string, password: string) => {
-  const auth = getAuth();
+const registerStandard = async (
+  email: string,
+  password: string
+): Promise<any> => {
+  const init: RequestInit = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  };
+  return await fetch(`${import.meta.env.VITE_DEV_API_URI}/users/signup`, init)
+    .then((res: Response) => {
+      return res.json();
+    })
+    .then((payload) => {
+      if (!payload.ok) {
+        return {
+          serverMessage: payload.message,
+        };
+      }
+
+      storeAccessTokenIntoLocalStorage(payload.access_token);
+      storeRefreshTokenIntoLocalStorage(payload.refresh_token);
+      let user = {
+        accessToken: payload.access_token,
+        refreshToken: payload.refresh_token,
+        email: payload.email ? payload.email : "",
+        _id: payload._id,
+      };
+
+      return {
+        ok: true,
+        user,
+        serverMessage: "Your are registered and connected !",
+      };
+    })
+    .catch((err) => {
+      return {
+        serverMessage: "An error occured",
+        error: err,
+      };
+    });
+
+  /* const auth = getAuth();
   let result = {
     user: null as null | KtmUser,
     serverMessage: null as null | string,
@@ -45,11 +81,13 @@ const registerStandard = async (email: string, password: string) => {
         ...result,
         errorCode: handleSigninError(error),
       };
-    });
+    }); */
 };
 
 const signinStandard = async (email: string, password: string) => {
-  const auth = getAuth();
+  console.log(email);
+
+  /* const auth = getAuth();
   let result = { user: null, serverMessage: null, errorCode: null };
   let user;
   return signInWithEmailAndPassword(auth, email, password)
@@ -73,11 +111,11 @@ const signinStandard = async (email: string, password: string) => {
         ...result,
         errorCode: handleSigninError(error),
       };
-    });
+    }); */
 };
 
-const oAuthLogin = async () => {
-  let result = { user: null, serverMessage: null, errorCode: null };
+/*const oAuthLogin = async () => {
+   let result = { user: null, serverMessage: null, errorCode: null };
   const provider = new GoogleAuthProvider();
   let user;
   return signInWithPopup(getAuth(), provider)
@@ -107,9 +145,9 @@ const oAuthLogin = async () => {
         errorCode: handleSigninError(error),
       };
     });
-};
+}; */
 
-function handleSigninError(error: any) {
+/* function handleSigninError(error: any) {
   switch (error.code) {
     case "auth/invalid-email":
       return "Invalid email or password was incorrect";
@@ -120,10 +158,14 @@ function handleSigninError(error: any) {
     default:
       return "Invalid email or password was incorrect";
   }
+} */
+
+function storeAccessTokenIntoLocalStorage(accTok: string) {
+  localStorage.setItem("access_token", JSON.stringify(accTok));
 }
 
-function storeUserIntoLocalStorage(user: any) {
-  localStorage.setItem("usr", JSON.stringify(user));
+function storeRefreshTokenIntoLocalStorage(rfrs_tok: string) {
+  localStorage.setItem("refresh_token", JSON.stringify(rfrs_tok));
 }
 
 function removeUserFromLocalStorage() {
@@ -141,6 +183,5 @@ function getUserFromLocalStorage(): any {
 export default {
   registerStandard,
   signinStandard,
-  oAuthLogin,
-  getUserFromLocalStorage
+  getUserFromLocalStorage,
 };
